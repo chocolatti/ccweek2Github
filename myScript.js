b0cdcc48218672b42f1982ee2de6d1e759494f87
Beibei He <chocolatti@outlook.com>
2026-09-11 12:22:30 -0400

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,15 @@
+
+const images = document.querySelectorAll("img");
+
+images.forEach(function (image) {
+    image.addEventListener("click", function () {
+        image.classList.toggle("enlarged");
+    });
+});
+
+function toggleInfo() {
+
+    const info = document.getElementById ("director-moreinformation");
+    info.classList.toggle("show");
+
+}
